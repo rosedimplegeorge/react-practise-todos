131d426558a0ba5e5c2f080073a6ac5e99e6f6b4
rosedimple <rosedimplegeorge@gmail.com>
2019-04-17 16:38:45 -0400

    Commit before PropType

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
 
 class Todos extends Component {
 
     render() {
         console.log(this.props.todos)
-        return (
-            <div>
-                <h1>Todos</h1>
-            </div>
-        );
+        return this.props.todos.map((todo) => (
+            // <h3>{todos.title}</h3>
+            <TodoItem key={todo.id} todo={todo}/>
+        ))
     }
 }
 
