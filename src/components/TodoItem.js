7e582a580e24153ec5962cc730d9d5932a0a41f5
rosedimple <rosedimplegeorge@gmail.com>
2019-04-18 13:54:42 -0400

    PropTypes have added in the components

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 
 class TodoItem extends Component {
     render() {
@@ -8,6 +10,11 @@ class TodoItem extends Component {
             </div>
         );
     }
+}
+
+//PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
 }
 
 export default TodoItem;
