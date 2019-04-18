57d6b8c67e6e69d50a8e3ebd88c59043d273d3eb
rosedimple <rosedimplegeorge@gmail.com>
2019-04-18 16:29:58 -0400

    Button style added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,13 +20,21 @@ class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/>
                 {title}
+                <button style={btnStyle}>x</button>
                 </p>
             </div>
         );
     }
 }
 
-
+const btnStyle= {
+    backgroundColor: 'red',
+    border: 'none',
+    padding: '5px 10px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
+}
 //PropTypes
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
