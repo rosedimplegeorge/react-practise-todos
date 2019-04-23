5b1dbdc1b2cdf1c686b65292629733e18726257d
rosedimple <rosedimplegeorge@gmail.com>
2019-04-23 13:58:06 -0400

    Linking the Different Routes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
         return (
             <div style={headerStyle}>
                 <h1>Todo List</h1>
+                <Link style={linkStyle}to="/">Home</Link> | <Link style={linkStyle} to="/About">About</Link>
             </div>
         );
     }
@@ -15,6 +17,11 @@ const headerStyle = {
     color: '#fff',
     textAlign: 'center',
     padding: '10px'
+}
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header;
